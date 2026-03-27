2ddd389e3babf449089f4eee31cbe15473746824
Bhatiya Kavya Vishnukumar <kavyabhatiya44@gmail.com>
2026-03-27 12:07:08 +0530

    Refactor clustering code in Jupyter notebook and add graph coloring algorithm in Python script
    - Updated execution counts and outputs in the Jupyter notebook for consistency.
    - Modified clustering logic to implement a more efficient k-means algorithm with loss tracking.
    - Added visualization of clustering results using matplotlib.
    - Introduced a new Python script for graph coloring using backtracking, defining a graph structure and implementing a safe coloring check.

---
+++ lab-11/q1.py
@@ -0,0 +1,88 @@
+graph = {
+
+    "Kutch": ["Banaskantha", "Surendranagar", "Jamnagar"],
+
+    "Banaskantha": ["Kutch", "Patan", "Sabarkantha"],
+
+    "Patan": ["Banaskantha", "Mehsana", "Surendranagar"],
+
+    "Mehsana": ["Patan", "Sabarkantha", "Gandhinagar", "Ahmedabad"],
+
+    "Sabarkantha": ["Banaskantha", "Mehsana", "Gandhinagar"],
+
+    "Gandhinagar": ["Mehsana", "Sabarkantha", "Ahmedabad", "Kheda"],
+
+    "Ahmedabad": ["Gandhinagar", "Mehsana", "Kheda", "Anand", "Surendranagar"],
+
+    "Kheda": ["Gandhinagar", "Ahmedabad", "Anand", "Panchmahal"],
+
+    "Anand": ["Ahmedabad", "Kheda", "Vadodara"],
+
+    "Vadodara": ["Anand", "Panchmahal", "Narmada", "Bharuch"],
+
+    "Panchmahal": ["Kheda", "Vadodara", "Dahod"],
+
+    "Dahod": ["Panchmahal"],
+
+    "Narmada": ["Vadodara", "Bharuch"],
+
+    "Bharuch": ["Vadodara", "Narmada", "Surat"],
+
+    "Surat": ["Bharuch", "Navsari"],
+
+    "Navsari": ["Surat", "Valsad", "Dangs"],
+
+    "Valsad": ["Navsari", "Dangs"],
+
+    "Dangs": ["Valsad", "Navsari"],
+
+    "Surendranagar": ["Kutch", "Patan", "Ahmedabad", "Rajkot"],
+
+    "Rajkot": ["Surendranagar", "Jamnagar", "Junagadh", "Amreli"],
+
+    "Jamnagar": ["Kutch", "Rajkot", "Porbandar"],
+
+    "Porbandar": ["Jamnagar", "Junagadh"],
+
+    "Junagadh": ["Rajkot", "Porbandar", "Amreli"],
+
+    "Amreli": ["Rajkot", "Junagadh", "Bhavnagar"],
+
+    "Bhavnagar": ["Amreli", "Ahmedabad"]
+}
+
+colors = ['r','g', 'b', 'y']
+
+
+def is_safe(node, color, assignment, graph):
+    for neighbor in graph[node]:
+        if neighbor in assignment and assignment[neighbor] == color:
+            return False
+    
+    return True
+
+def solve(graph, assignment= {}):
+
+    if len(graph) == len(assignment):
+        return assignment
+    
+    node = list(graph.keys())[len(assignment)]
+    for color in colors:
+
+        if is_safe(node,color, assignment, graph):
+            assignment[node] = color
+            result = solve(graph,assignment)
+            if result:
+                return result
+            del assignment[node]
+
+    return None
+
+solution = solve(graph)
+print(solution)
+
+
+    
+
+
+    
